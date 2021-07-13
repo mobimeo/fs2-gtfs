b890b90f632cc665f970a9c6f542c6181577d9e4
Lucas Satabin <lucas.satabin@mobimeo.com>
2021-07-13 11:11:22 +0200

    Fix examples

--- examples/src/main/scala/ListStopNames.scala
+++ examples/src/main/scala/ListStopNames.scala
@@ -2,7 +2,7 @@ package test
 
 import cats.effect._
 import cats.syntax.all._
-import com.mobimeo.gtfs._
+import com.mobimeo.gtfs.file._
 import java.nio.file.Paths
 
 object ListStopNames extends IOApp {
@@ -10,7 +10,7 @@ object ListStopNames extends IOApp {
     args match {
       case Nil => IO(println("please provide the path to the GTFS file as argument")).as(ExitCode.Error)
       case path :: _ =>
-        Gtfs[IO](Paths.get(path))
+        GtfsFile[IO](Paths.get(path))
           .use { gtfs =>
             gtfs.read.rawStops.map(stop => stop("stop_name").filter(_.nonEmpty)).unNone.compile.toList
           }

fc73cf4e675a1160cc79fe73e7dca3816108e669
Daniel Karch <daniel.karch@mobimeo.com>
2021-07-15 13:31:58 +0200

    DB backend

--- db/src/main/scala/com/mobimeo/gtfs/db/GtfsDb.scala
+++ db/src/main/scala/com/mobimeo/gtfs/db/GtfsDb.scala
@@ -2,9 +2,10 @@ package com.mobimeo.gtfs.db
 
 import doobie._, doobie.implicits._
 import cats.effect._
-import com.mobimeo.gtfs.Gtfs
-import com.mobimeo.gtfs.model.Agency
 import cats.syntax.all._
+import com.mobimeo.gtfs.GtfsRead
+import com.mobimeo.gtfs
+import fs2.Pipe
 
 object GtfsDb {
 
@@ -16,9 +17,46 @@ object GtfsDb {
 
   val createTables = Tables.Agency.create
 
-  def create[F[_]: Async](xa: Transactor[F], gtfs: Gtfs[F]) =
-    createTables.update.run.transact(xa) *> {
-      gtfs.read.agencies[Agency].evalMap(agency => Tables.Agency.insert(agency).update.run.transact(xa)).compile.drain
-    }
+  def create[F[_]: Async](xa: Transactor[F]): F[GtfsDb[F]] =
+    createTables.update.run.transact(xa) as new GtfsDb(xa)        
+}
+
+final class GtfsDb[F[_]: Async](xa: Transactor[F]) extends gtfs.Gtfs[F, Read, Write] {
+  def hasFile(name: String): F[Boolean] = ???
+
+  object read extends GtfsDbRead[F](xa)
+
+  object write extends GtfsDbWrite[F](xa)
+}
+
+abstract class GtfsDbRead[F[_]: Async](xa: Transactor[F]) extends GtfsRead[F, Read] {
+
+  val y = xa.yolo
+  import y._
+
+  private def select[R: Read](table: String) =
+    sql"select * from ${Fragment.const(table)}".query[R]
+
+  def file[R: Read](name: String): fs2.Stream[F, R] = {
+      val q = select(name)
+      val s = fs2.Stream.eval[F, Unit](q.check).drain
+      s ++ q.stream.transact(xa)
+  }
+}
+
+abstract class GtfsDbWrite[F[_]: Async](xa: Transactor[F]) extends gtfs.GtfsWrite[F, Write] {
+
+  val y = xa.yolo
+  import y._
+
+  private def insert[W: Write](table: String, w: W) =
+    sql"insert into ${Fragment.const(table)} values (${Fragments.values(w)})".update
+
+
+  def file[W: Write](name: String): Pipe[F, W, Nothing] =
+    _.evalTap { w => 
+        val q = insert(name, w)
+        q.check *> q.run.transact(xa)
+    }.drain
 
 }
--- db/src/main/scala/com/mobimeo/gtfs/db/TestApp.scala
+++ db/src/main/scala/com/mobimeo/gtfs/db/TestApp.scala
@@ -3,26 +3,23 @@ package com.mobimeo.gtfs.db
 import cats.effect.IOApp
 import cats.effect.IO
 
-import doobie.implicits._
 import cats.effect.ExitCode
-import com.mobimeo.gtfs.Gtfs
 import java.nio.file.Paths
 import com.mobimeo.gtfs.model.Agency
+import com.mobimeo.gtfs.file.GtfsFile
 
 object TestApp extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] = {
 
     val xa = GtfsDb.xa[IO]
-
-    // val y = xa.yolo
-    // import y._
-    // Tables.Agency.insert(Agency("id", "name", "url", ZoneId.of("UTC"), None, None, None, None)).update.check *>
-
-    Gtfs[IO](Paths.get(this.getClass().getClassLoader().getResource("google_transit.zip").toURI())).use { gtfs =>
-      GtfsDb.create(xa, gtfs) *> {
-        val select = sql"select * from agency where agency_id='1'".query[Agency].unique
-        select.transact(xa).flatMap(agency => IO.println(agency.toString)) as ExitCode.Success
+       
+    GtfsFile[IO](Paths.get(this.getClass().getClassLoader().getResource("google_transit.zip").toURI())).use { gtfs =>
+      GtfsDb.create(xa).flatMap { gtfsDb =>
+        val copy = gtfs.read.file[Agency]("agency.txt") through gtfsDb.write.file[Agency]("agency")
+        val read = gtfsDb.read.file[Agency]("agency")
+        
+        (copy ++ read).evalTap(agency => IO.println(agency.toString)).compile.drain as ExitCode.Success
       }
     }
   }

77f23340bc4d39ed2311eaded4fcaaeee2701849
Yannick Heiber <yannick.heiber@mobimeo.com>
2021-07-27 12:23:00 +0200

    Optimize CsvIntEnum lookup on Scala 3

--- core/src/main/scala-3/com/mobimeo/gtfs/model/CsvEnum.scala
+++ core/src/main/scala-3/com/mobimeo/gtfs/model/CsvEnum.scala
@@ -13,10 +13,10 @@ trait CsvEnum[T <: EnumEntry] {
 
   def values: Array[T]
 
+  lazy val byEntryName: Map[String, T] = values.toList.groupMapReduce(_.entryName)(identity)((e, _) => e)
+
   implicit val cellDecoder: CellDecoder[T] =
-    CellDecoder.stringDecoder.emap(s =>
-      values.find(_.entryName == s).toRight(new DecoderError(s"Unknown enum value $s"))
-    )
+    CellDecoder.stringDecoder.emap(s => byEntryName.get(s).toRight(new DecoderError(s"Unknown enum value $s")))
 
   implicit val cellEncoder: CellEncoder[T] =
     CellEncoder.stringEncoder.contramap(_.entryName)
@@ -31,12 +31,17 @@ trait CsvIntEnum[T <: IntEnumEntry] {
 
   def values: Array[T]
 
-  def valueOf(n: Int): Option[T] = values.find(_.value == n)
+  // Build an array for quick int->value lookup using the fact GTFS has small known key spaces
+  // Performance optimization for decoding a lot of T instances as we don't have to search the value space each time
+  private lazy val valueLookup: Array[Option[T]] = {
+    val grouped = values.toList.groupBy(_.value)
+    Array.tabulate(grouped.keySet.max + 1)(grouped.get(_).flatMap(_.headOption))
+  }
+
+  def valueOf(n: Int): Option[T] = valueLookup(n)
 
   implicit val cellDecoder: CellDecoder[T] =
-    CellDecoder.intDecoder.emap(n =>
-      valueOf(n).toRight(new DecoderError(s"Unknown enum value $n"))
-    )
+    CellDecoder.intDecoder.emap(n => valueOf(n).toRight(new DecoderError(s"Unknown enum value $n")))
 
   implicit val cellEncoder: CellEncoder[T] =
     CellEncoder.intEncoder.contramap(_.value)

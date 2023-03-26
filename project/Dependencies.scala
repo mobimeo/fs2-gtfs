50f7e40c55a6ba4abec45d83ba8cedc1e40f68f7
Scala Steward <scala_steward@virtuslab.com>
2023-03-26 15:38:14 +0000

    Update fs2-data-csv, fs2-data-csv-generic to 1.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Versions {
   val fs2        = "3.3.0"
-  val fs2Data    = "1.6.1"
+  val fs2Data    = "1.7.0"
   val enumeratum = "1.7.0"
   val weaver     = "0.8.1"
   val circe      = "0.14.3"

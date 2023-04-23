e5a3e31114b551309274f106394746199802ba24
Scala Steward <scala_steward@virtuslab.com>
2023-04-23 16:30:39 +0000

    Update log4cats-core to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Versions {
   val enumeratum = "1.7.0"
   val weaver     = "0.8.1"
   val circe      = "0.14.3"
-  val log4cats   = "2.5.0"
+  val log4cats   = "2.6.0"
   val catsParse  = "0.3.9"
   val literally  = "1.1.0"
 }
